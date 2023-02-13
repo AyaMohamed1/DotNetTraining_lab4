81d1e29da26e7706c5cafd321fbd2d85eef6f435
Aya Ibrahim <aya.ibrahim@itworx.com>
2023-02-13 22:28:50 +0200

    Add project files.

---
+++ lab4/BookFunctions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4
+{
+    public class BookFunctions {
+        public static string GetTitle(Book B) {
+            return B.Title;
+        }
+        public static string GetAuthors(Book B) {
+            string res = "Book Authors: ";
+            foreach (var item in B.Authors) { res += item.ToString() + ", "; }
+            return res;
+        }
+        public static string GetPrice(Book B) {
+            return B.Price.ToString();
+        }
+    }
+}
